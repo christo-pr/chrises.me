77b803c1b8c0bc581852a92001841cfa2a61ff08
Christofer Flores <cristofer.flort@gmail.com>
2022-02-10 14:27:27 -0600

    Created projects page

---
+++ app/assets/projects/index.js
@@ -0,0 +1,11 @@
+import dangerouslySetHtmlContent from './dangerously-set-html-content.png'
+import laCarlota from './la-carlota.png'
+import reactLegra from './react-legra.png'
+import reduxHunt from './reducks-hunt.png'
+
+export default {
+  laCarlota,
+  reduxHunt,
+  dangerouslySetHtmlContent,
+  reactLegra,
+}
---
+++ app/routes/projects.jsx
@@ -0,0 +1,61 @@
+import { BiLinkExternal as ExternalIcon } from 'react-icons/bi'
+import projectsImages from '~/assets/projects'
+
+export default function Projects() {
+  const projectSettings = {
+    reduxHunt: {
+      title: 'Redux Hunt',
+      text: "Want to learn how Redux works? How about a game? Redux-Hunt its exactly what you need! Gotta catch 'em all!",
+      url: 'https://christo-pr.github.io/redux-jsconf/',
+    },
+    laCarlota: {
+      title: 'La Carlota - Colima',
+      text: 'non-profit site that list a few of restaurants that are in a little city in Mexico, called Colima (where I live). Definitely check it out if your near here',
+      url: 'https://lacarlota.chrises.me/',
+    },
+    reactLegra: {
+      title: 'React Legra',
+      text: 'Fun library that takes the power of legraJS and give the ability to render lego-like component using React.',
+      url: 'https://christo-pr.github.io/react-legra-site/',
+    },
+    dangerouslySetHtmlContent: {
+      title: 'Dangerously Set Html Content',
+      text: 'Small react library that allows you to render any valid html string that can contain a script it will execute it.',
+      url: 'https://www.npmjs.com/package/dangerously-set-html-content',
+    },
+  }
+  const projects = Object.keys(projectsImages).map((p) => ({
+    name: p,
+    icon: projectsImages[p],
+    ...projectSettings[p],
+  }))
+
+  return (
+    <div className="px-5 md:px-0">
+      <h2 className="text-base lg:text-xl">Things I've done:</h2>
+      <ul className="grid grid-cols-1 md:grid-cols-3 auto-rows-min gap-20 mt-20">
+        {projects.map((p) => (
+          <li key={p.name} className="bg-white bg-opacity-10 rounded pb-4">
+            <a
+              href={p.url}
+              target="_blank"
+              className="hover:text-primary transition-colors"
+            >
+              <figure className="mb-10 border-b">
+                <img
+                  src={p.icon}
+                  alt={p.name}
+                  className="rounded-tl rounded-tr mx-auto"
+                />
+                <figcaption className="mt-4 px-2 flex items-center gap-2">
+                  {p.title} <ExternalIcon size={12} />
+                </figcaption>
+              </figure>
+              <p className="text-white px-2">{p.text}</p>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
